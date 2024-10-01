364112b5bce061488a3aa0d314db0ba0c7a05c5f
domo <davidas99017@gmail.com>
2024-10-01 15:33:32 -0400

    update

--- scripts/randWall.sh
+++ scripts/randWall.sh
@@ -5,11 +5,11 @@ wal_path=$(which wal)
 pywalfox_path=$(which pywalfox)
 
 # replace path with your wallpaper directory containing images
-wall_arr=("$HOME/Pictures/wallpapers/Abstract"/*)
+wall_arr=("$HOME/Pictures/wallpapers/pywal_wall"/*)
 randInd=$((RANDOM % ${#wall_arr[@]}))
 randWall="${wall_arr[$randInd]}"
 swww img "$randWall" --transition-type center --transition-step 60 --transition-fps 120
 sleep 0.3
-${wal_path} -i "$randWall" --backend colorz
+${wal_path} -i "$randWall" --backend wall
 sleep 0.1
 ${pywalfox_path} update
