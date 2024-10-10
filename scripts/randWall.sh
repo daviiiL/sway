8a286ceb92e3ff24f468e10b08399bf8bfbc3343
titus <davidas99017@gmail.com>
2024-10-10 05:35:18 -0400

    test colorscheme changes

--- scripts/randWall.sh
+++ scripts/randWall.sh
@@ -1,23 +1,23 @@
 #!/bin/zsh
 
 # find command paths
-wal_path=$(which wal)
-pywalfox_path=$(which pywalfox)
+# pywalfox_path=$(which pywalfox)
 
 wall_arr=("$HOME/Pictures/wallpapers/pywal_wall"/*)
 
 random_index=$((RANDOM % ${#wall_arr[@]}))
 random_wallpaper="${wall_arr[$random_index]}"
 
-swww img "$random_wallpaper" --transition-type center --transition-step 60 --transition-fps 120
+matugen image $random_wallpaper
+# swww img "$random_wallpaper" --transition-type center --transition-step 60 --transition-fps 120
 
-sleep 0.3
+# sleep 0.3
 
-${wal_path} -i "$random_wallpaper" --backend wall
+# ${wal_path} -i "$random_wallpaper" --backend wall
 
-sleep 0.1
+# sleep 0.1
 
-${pywalfox_path} update
+# ${pywalfox_path} update
 # swaymsg reload
 killall waybar
 waybar & disown
